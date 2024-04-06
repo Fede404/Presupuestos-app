a28f41d044bfeb3fc02ddbe5a2b1a42c79757484
fedeperezj <federicoperezj@gmail.com>
2024-04-06 20:57:35 -0300

    10 - Añadido div de hoja y pequeños cambios

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
-// VARIABLES Y FUNCIONES PARA ABRIR Y CERRAR LISTA DE NAV (SELECCIONAR PRODUCTO)
+// VARIABLES DIVS MODALES Y LISTA DE PRODUCTO
 let prodDiv = document.querySelector("#prodList");
+let secVidrios = document.querySelector(".vidrio-sec");
+let secProducto = document.querySelector(".prod-sec")
+let secPrecios = document.querySelector(".precios-sec");
+let secPresupuestos = document.querySelector(".hojaPresupuesto-sec")
 
+//FUNCIÓN ALTERNAR DIV LISTA DE PRODUCTOS CON BOTÓN DE NAV
 let displayChange = function () {
 	if (prodDiv.style.display === "none") {
 		prodDiv.style.display = "flex";
@@ -9,12 +14,7 @@ let displayChange = function () {
 	}
 };
 
-// VARIABLES Y FUNCIONES PARA ABRIR Y CERRAR MODALLES 
-let secVidrios = document.querySelector(".vidrio-sec");
-let secProducto = document.querySelector(".prod-sec")
-let secPrecios = document.querySelector(".precios-sec");
-let secPresupuestos = document.querySelector(".hojaPresupuesto-sec")
-
+// FUNCIÓN ALTERNAR ENTRE DIVS PRINCIPAL Y VIDRIOS CON BOTÓN DE NAV
 let displayChange2 = function () {
 	if (secVidrios.style.display === "none") {
 		secVidrios.style.display = "flex";
@@ -28,12 +28,16 @@ let displayChange2 = function () {
 		secPresupuestos.style.display = "none";
 	}
 };
+
+// FUNCIÓN CERRAR MODAL VIDRIO CON BOTÓN VOLVER
 let cerrarModalVidrio = function() {
 	secVidrios.style.display = "none";
 	secProducto.style.display = "flex";
 	secPrecios.style.display = "none";
 	secPresupuestos.style.display = "none";
 }
+
+// FUNCIÓN ALTERNAR ENTRE DIVS PRINCIPAL Y PRCIOS CON BOTÓN DE NAV
 let displayChange3 = function () {
 	if (secPrecios.style.display === "none") {
 		secPrecios.style.display = "flex";
@@ -47,6 +51,8 @@ let displayChange3 = function () {
 		secPresupuestos.style.display = "none";
 	}
 };
+
+// FUNCIÓN CERRAR MODAL DE PRECIO
 let cerrarModalPrecio = function() {
 	secVidrios.style.display = "none";
 	secProducto.style.display = "flex";
@@ -54,13 +60,15 @@ let cerrarModalPrecio = function() {
 	secPresupuestos.style.display = "none";
 }
 
-let displayChange4 = function() {
+// FUNCIÓN ABRIR MODAL PRESUPUESTO CON BOTÓN DE HEADER
+let abrirModalPresupuesto = function() {
 	secVidrios.style.display = "none";
 	secProducto.style.display = "none";
 	secPrecios.style.display = "none";
 	secPresupuestos.style.display = "flex";
 }
 
+// FUNCIÓN CERRAR MODAL PRESUPUESTO
 let cerrarModalPresupuesto = function() {
 	secVidrios.style.display = "none";
 	secProducto.style.display = "flex";
